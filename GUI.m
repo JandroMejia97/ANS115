001306a7bc3a4bd9ebfe0560238604256506b0fc
Carlos Alejandro Hernández Mejía <32623757+JandroMejia97@users.noreply.github.com>
2018-06-07 14:22:56 -0600

    Resolviendo Merge

--- GUI.m
+++ GUI.m
@@ -1,27 +1,27 @@
 clc;clear;
 
 function longitud=getLongitud(hObject)
-  longitud=str2num(get(hObject,'string'));
+  longitud=str2num(get(hObject,'string'))
 endfunction
 
 function gravedad=getGravedad(hObject)
-  gravedad=str2num(get(hObject,'string'));
+  gravedad=str2num(get(hObject,'string'))
 endfunction
 
 function inferior=getInferior(hObject)
-  inferior=str2num(get(hObject,'string'));
+  inferior=str2num(get(hObject,'string'))
 endfunction
 
 function superior=getSuperior(hObject)
-  superior=str2num(get(hObject,'string'));
+  superior=str2num(get(hObject,'string'))
 endfunction
 
 function iteraciones=getIteraciones(hObject)
-  iteraciones=str2num(get(hObject,'string'));
+  iteraciones=str2num(get(hObject,'string'))
 endfunction
 
 function puntoInicial=getPuntoInicial(hObject)
-  puntoInicial=str2num(get(hObject,'string'));
+  puntoInicial=str2num(get(hObject,'string'))
 endfunction
 
 f = dialog ("name", "Ecuación Deferencial No Lineal", "position", [250 180 825 420]);
@@ -30,38 +30,23 @@ gp1 = uibuttongroup (f, "Position", [ 0.05 0.47 0.55 0.4]);
 lblTema = uicontrol (f, "style", "text", "string", "SOLUCIÓN NUMÉRICA DE LA ECUACIÓN DIFERENCIAL NO LINEAL QUE DESCRIBE LAS OSCILACIONES DEL PÉNDULO SIMPLE ", "position",[7 367 800 70]);
 lblIngresar = uicontrol (gp1, "style", "text", "string", "Ingresar Datos", "position",[0 135 120 30]);
 lblLongitud = uicontrol (gp1, "style", "text", "string", "Longitud:", "position",[0 95 120 22]);
-txtLongitud = uicontrol (gp1, "style", "edit", "position",[125 95 80 22],'callback',@getLongitud);
+txtLongitud = uicontrol (gp1, "style", "edit",'callback',@getLongitud, "position",[125 95 80 22]);
 lblGravedad = uicontrol (gp1, "style", "text", "string", "Gravedad:", "position",[0 60 120 22]);
-txtGravedad = uicontrol (gp1, "style", "edit", "position",[125 60 80 22],'callback',@getGravedad);
+txtGravedad = uicontrol (gp1, "style", "edit",'callback',@getGravedad, "position",[125 60 80 22]);
 lblIte = uicontrol (gp1, "style", "text", "string", "N° de iteraciones:", "position",[0 25 120 22 ]);
-txtIte = uicontrol (gp1, "style", "edit", "position",[125 25 80 22 ],'callback',@getIteraciones);
+txtIte = uicontrol (gp1, "style", "edit",'callback',@getIteraciones, "position",[125 25 80 22 ]);
 lblInferior = uicontrol (gp1, "style", "text", "string", "Límite inferior:", "position",[225 95 120 22]);
-txtInferior = uicontrol (gp1, "style", "edit", "position",[350 95 80 22],@getInferior);
+txtInferior = uicontrol (gp1, "style", "edit","callback",@getInferior, "position",[350 95 80 22]);
 lblSuperior = uicontrol (gp1, "style", "text", "string", "Límite superior:", "position",[225 60 120 22]);
-txtSperior = uicontrol (gp1, "style", "edit", "position",[350 60 80 22],@getSuperior);
+txtSperior = uicontrol (gp1, "style", "edit","callback",@getSuperior,"position",[350 60 80 22]);
 lblPuntoInicial = uicontrol (gp1, "style", "text", "string", "Valor inicial:", "position",[225 25 120 22]);
-txtPuntoInicial = uicontrol (gp1, "style", "edit", "position",[350 25 80 22 ],@getPuntoInicial);
-
-lblEleccion = uicontrol (f, "style", "text", "string", "Elegir Método Númerico", "position",[30 190 150 40]);
-gp = uibuttongroup(f,"Title","Seleccione una opcion", "position",[30 190 150 240]);
-c1 = uicontrol(gp, "style", "radiobutton", "string", "Método de Euler", "position",[50 160 150 40]);
-c2 = uicontrol(gp, "style", "radiobutton", "string", "Método del Punto Medio", "position",[50 130 175 40]);
-c3 = uicontrol(gp, "style", "radiobutton", "string", "Método de Heun", "position",[50 100 150 40]);
-c4 = uicontrol(gp, "style", "radiobutton", "string", "Método de Runge-Kutta", "position",[50 70 175 40]);
-c5 = uicontrol(gp, "style", "radiobutton", "string", "Todos los anteriores", "position",[50 40 225 40]);
-          % create figure without a default toolbar
-          
-% create a button (default style)
-b1 = uicontrol (f, "string", "Resolver", "position",[80 10 150 40], "callback",@resolver);
-b2 = uicontrol (f, "string", "Ver gráficos", "position",[250 10 150 40]);
-
+txtPuntoInicial = uicontrol (gp1, "style", "edit","callback",@getPuntoInicial, "position",[350 25 80 22 ]);
 
 %c1 = uicontrol (f, "style", "radiobutton", "string", "Método de Euler", "position",[50 110 150 40]);
 %c2 = uicontrol (f, "style", "radiobutton", "string", "Método del Punto Medio", "position",[250 80 175 40]);
 %c3 = uicontrol (f, "style", "radiobutton", "string", "Método de Heun", "position",[50 80 150 40]);
 %c4 = uicontrol (f, "style", "radiobutton", "string", "Método de Runge-Kutta", "position",[50 50 175 40]);
 %c5 = uicontrol (f, "style", "radiobutton", "string", "Todos los anteriores", "position",[250 50 225 40]);
-
 
 gp = uibuttongroup (f, "Position", [ 0.65 0.47 0.29 0.4]);
 lblEleccion = uicontrol (gp, "style", "text", "string", "Elegir Método Númerico", "position",[20 135 150 30]);
@@ -81,6 +66,9 @@ b4 = uicontrol (gp, "style", "radiobutton", ...
 b5 = uicontrol (gp, "style", "radiobutton", ...
                 "string", "Todos los anteriores", ...
                 "Position", [ 40 0 225 30 ]);
+
+b1 = uicontrol (f, "string", "Resolver", "position",[250 130 150 40]);
+b2 = uicontrol (f, "string", "Ver gráficos", "position",[420 130 150 40]);
 
 %[T, L_X] = table (X)
 %[T, Valor, Error] = table (2, 4);
